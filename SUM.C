2cb474bde8f99e06d60c2a1cfcfabe2d47f15f04
Swastikajaiswal <161068626+Swastikajaiswal@users.noreply.github.com>
2024-09-25 12:14:07 +0530

    Add files via upload

---
+++ SUM.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	printf("\nenter first number");
+	scanf("%d",&a);
+	printf("\nenter second number");
+	scanf("%d",&b);
+	printf("sum of %d and %d is %d",a,b,a+b);
+	getch();
+}
