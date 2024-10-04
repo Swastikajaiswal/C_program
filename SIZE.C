0ae6fc779d57ef40c04d43936abf95b5a4800f40
Swastikajaiswal <161068626+Swastikajaiswal@users.noreply.github.com>
2024-10-04 11:34:42 +0530

    Add files via upload

---
+++ SIZE.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	printf("size of int: %u bytes\n", sizeof(int));
+	printf("size of int: %u bytes\n", sizeof(double));
+	printf("size of int: %u bytes\n", sizeof(float));
+	printf("size of int: %u bytes\n", sizeof(char));
+
+}
